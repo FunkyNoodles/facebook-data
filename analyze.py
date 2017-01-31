23693a3b9763287cb947fb26ce1f95d398d81b97
Funky Noodles <nickasds@hotmail.com>
2017-01-31 15:49:08 -0600

    Added histogram

--- analyze.py
+++ analyze.py
@@ -1,6 +1,9 @@
 from dateutil.parser import *
 import json
 import time
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
 
 json_file_name = 'D://Facebook/2016-01-28/json/messages.json'
 
@@ -12,6 +15,20 @@ with open(json_file_name, 'r') as infile:
 t1 = time.time()
 print 'Reading data from JSON took:', (t1 - t0), 's'
 
-for people, message_thread in allMessages.iteritems():
-    print people, len(allMessages[people])
+# for people, message_thread in allMessages.iteritems():
+#     print people, len(allMessages[people])
 
+# Computing histogram
+people = 'Islam Kadri, Louis Lu'
+
+t0 = time.time()
+hist, bin_edges = np.histogram([parse(message['time']).hour for message in allMessages[people]], bins=range(25))
+t1 = time.time()
+print 'Computing histogram took:', (t1 - t0), 's'
+
+# Plot the histogram
+plt.bar(range(24), hist, color='r')
+plt.title('Number of messages vs. hour between ' + people)
+plt.xlabel('Hour')
+plt.ylabel('Number of messages')
+plt.show()
